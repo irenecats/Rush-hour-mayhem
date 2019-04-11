10575b8443199e15b0e18ba0ace87684e7e3535e
monineun <moni28198@gmail.com>
2019-04-11 18:19:53 +0200

    Clase bala creada y arreglos en update y render
    Disparo de Bala añadido al jugador. Actualizacion de los metodos update y render para la correcta interpolacion

---
+++ include/Bala.h
@@ -0,0 +1,37 @@
+#ifndef BALA_H
+#define BALA_H
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Sprite.h"
+#include "TexturaContainer.h"
+#include "Interpolacion.h"
+#include "Bala.h"
+#include "Clock.h"
+#include "Window.h"
+
+
+class Bala
+{
+    public:
+        Bala(float _x, float _y,float rot, float _dirx, float _diry);
+
+        void update();
+        void render(Window&, float ptick);
+
+        bool getaborrar();
+        Sprite getBala();
+
+
+    private:
+        Sprite bala;
+        Interpolacion lastState;
+        Interpolacion newState;
+
+        float dirx, diry;
+
+        Clock clock;
+        bool aborrar = false;
+
+};
+
+#endif // BALA_H
---
+++ src/Bala.cpp
@@ -0,0 +1,52 @@
+#include "Bala.h"
+
+Bala::Bala(float _x, float _y,float rot, float _dirx, float _diry){
+
+    std::string url ("resources/spritesheet_vehiclesmod.png");
+    int pos = TexturaContainer::instancia()->crearTextura(url);
+
+    bala.setTextura(TexturaContainer::instancia()->getTextura(pos));
+
+    bala.setRectTextura(sf::IntRect(3*70 + 5, 3*131 - 12, 70, 128));
+    bala.setPosition(_x, _y);
+    bala.setRotation(rot);
+    bala.setScale(0.5f, 0.5f);
+    bala.setOrigin(35, 35);
+
+    dirx = _dirx;
+    diry = _diry;
+}
+
+
+void Bala::update(){
+
+    lastState.Setx(newState.Getx());
+    lastState.Sety(newState.Gety());
+
+    if(clock.getElapsedTime()<4000){
+        bala.mover(dirx*20, diry*20);
+    }else{
+        aborrar = true;
+    }
+
+    newState.Setx(bala.getPosition()[0]);
+    newState.Sety(bala.getPosition()[1]);
+
+}
+
+void Bala::render(Window &window, float ptick){
+    bala.setPosition(lastState.Getx()*(1-ptick) + newState.Getx()*(ptick), lastState.Gety()*(1-ptick) + newState.Gety()*(ptick));
+    window.draw(bala.getSprite());
+}
+
+
+
+Sprite Bala::getBala(){
+    return bala;
+}
+
+
+bool Bala::getaborrar(){
+    return aborrar;
+}
+
